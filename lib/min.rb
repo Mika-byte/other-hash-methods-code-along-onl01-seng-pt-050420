2e5414e18fd247a5ae945a9b779c1a83a088ebf2
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-16 06:31:29 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -9,13 +9,12 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  def get_the_values(groceries)
   groceries = {
   dairy: ["milk", "yogurt", "cheese"],
   vegetable: ["carrots", "broccoli", "cucumbers"],
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
 } 
- groceries.values.flatten
+ groceries.min
 
 end
